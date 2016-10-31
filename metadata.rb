35c6e6440b35eb7c1cdb0b5473b9cc825cb358de
Thomas Cate <tcate@chef.io>
2016-10-31 16:36:50 -0500

    initial commit

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license 'all_rights'
 description 'Installs/Configures dsc_test'
 long_description 'Installs/Configures dsc_test'
 version '0.1.0'
+
+depends 'powershell'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,30 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+include_recipe 'powershell'
+
+
+user 'tcate' do
+  action :create
+  comment 'Thomas Cate'
+  password 'p@ssw0rd'
+end
+
+group 'Remote Desktop Users' do
+  action :modify
+  members ['tcate']
+  append true
+end
+
+
+dsc_resource "Allow IIS Rule" do
+  resource :xfirewall
+  property :name, "Allow Webserver"
+  property :direction, "inbound"
+  property :action, "allow"
+  property :protocol, "tcp"
+  property :localport, [ "80", "443" ]
+  property :ensure, "Present"
+  property :enabled, "True"
+end
